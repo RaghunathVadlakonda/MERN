2093a8b3d1879f1dab4641858b1e4faf549179db
Raghunath <Vadlakonda.raghu5@gmail.com>
2020-04-21 14:20:26 +0530

    started node server.

---
+++ app.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.get('/', (req, res) => {
+    res.send("Home! This is sample.");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port`);
+});
