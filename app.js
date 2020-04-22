df710d7e1fbec98ffaf1d38e61dcbeeb98b60de6
Raghunath <Vadlakonda.raghu5@gmail.com>
2020-04-22 13:13:18 +0530

    connected to mongoDB atlas for cloud with mongoose.

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 require('dotenv').config()
 const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
+
+connectDB();
 
 const PORT = process.env.PORT;
 
---
+++ config/db.js
@@ -0,0 +1,24 @@
+// mongoose to connect with the MongoDB Database.
+const mongoose = require('mongoose');
+
+// config folder created for mention global variables. 
+const config = require('config');
+
+const db = config.get('mongoURI');
+
+// Using async and await here
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db,{useNewUrlParser:true})
+
+        console.log("Successfully connected to MongoDB.");
+    } catch(err)
+    {
+        console.error(err.message);
+        process.exit(1);
+
+    }
+};
+
+module.exports = connectDB;
